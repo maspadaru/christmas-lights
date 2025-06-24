dcbbdf2e3003ef732f778634bd5f1f73ddde8925
Mike Spadaru <mspadaru@gmail.com>
2025-06-24 08:22:55 +0200

    add test for turning brightness up for one light in the grid

--- src/test/java/org/mspadaru/BrightnessLightGridTest.java
+++ src/test/java/org/mspadaru/BrightnessLightGridTest.java
@@ -1,7 +1,35 @@
 package org.mspadaru;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BrightnessLightGridTest {
+
+    @Test
+    void testTurnOnOneLightOnce() {
+        BinaryLightGrid lightGrid = new BrightnessLightGrid((1000, 1000);
+        lightGrid.turnOnLights(new Point(0, 0), new Point(0, 0));
+        assertEquals(1, lightGrid.measureLights());
+    }
+
+    @Test
+    void testTurnOnOneLightTwoTimes() {
+        BinaryLightGrid lightGrid = new BrightnessLightGrid(1000, 1000);
+        lightGrid.turnOnLights(new Point(0, 0), new Point(0, 0));
+        lightGrid.turnOnLights(new Point(0, 0), new Point(0, 0));
+        assertEquals(2, lightGrid.measureLights());
+    }
+
+    @Test
+    void testTurnOnOneLightMaxTimes() {
+        BinaryLightGrid lightGrid = new BrightnessLightGrid(1000, 1000);
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            lightGrid.turnOnLights(new Point(0, 0), new Point(0, 0));
+        }
+        assertEquals(Integer.MAX_VALUE, lightGrid.measureLights());
+    }
+
+
 
 }
