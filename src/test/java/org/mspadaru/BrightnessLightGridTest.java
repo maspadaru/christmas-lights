c21bc6160c799a6f012eba3ff23297ff60a4be1d
Mike Spadaru <mspadaru@gmail.com>
2025-06-24 09:31:06 +0200

    add test for turnning brightness down

--- src/test/java/org/mspadaru/BrightnessLightGridTest.java
+++ src/test/java/org/mspadaru/BrightnessLightGridTest.java
@@ -30,5 +30,45 @@ class BrightnessLightGridTest {
         assertEquals(Integer.MAX_VALUE, lightGrid.measureLights());
     }
 
+    @Test
+    void testTurnOnAllLightsTenTimes() {
+        LightGrid lightGrid = new BrightnessLightGrid(1000, 1000);
+        for (int i = 0; i < 10; i++) {
+            lightGrid.turnOnLights(new Point(0, 0), new Point(999, 999));
+        }
+        assertEquals(10_000_000, lightGrid.measureLights());
+    }
+
+
+    @Test
+    void testTurnOffOneLightOnce() {
+        LightGrid lightGrid = new BrightnessLightGrid(1000, 1000);
+        lightGrid.turnOnLights(new Point(0, 0), new Point(0, 0));
+        lightGrid.turnOffLights(new Point(0, 0), new Point(0, 0));
+        assertEquals(0, lightGrid.measureLights());
+    }
+
+    @Test
+    void testTurnOffOneLightTwoTimes() {
+        LightGrid lightGrid = new BrightnessLightGrid(1000, 1000);
+        for (int i = 0; i < 10; i++) {
+            lightGrid.turnOnLights(new Point(0, 0), new Point(0, 0));
+        }
+        lightGrid.turnOffLights(new Point(0, 0), new Point(0, 0));
+        lightGrid.turnOffLights(new Point(0, 0), new Point(0, 0));
+        assertEquals(8, lightGrid.measureLights());
+    }
+
+    @Test
+    void testTurnOffAllLightsTenTimes() {
+        LightGrid lightGrid = new BrightnessLightGrid(1000, 1000);
+        for (int i = 0; i < 10; i++) {
+            lightGrid.turnOnLights(new Point(0, 0), new Point(999, 999));
+        }
+        for (int i = 0; i < 10; i++) {
+            lightGrid.turnOffLights(new Point(0, 0), new Point(999, 999));
+        }
+        assertEquals(0, lightGrid.measureLights());
+    }
 
 }
